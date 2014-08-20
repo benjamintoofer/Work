34eb162e5839f488c4355f441708f2e4b46168a6
Benjamin Toofer <benjamintoofer@gmail.com>
2014-08-20 13:09:51 -0700

    addidnd TestFx file

---
+++ TestFX/src/sample/TestConstants.java
@@ -0,0 +1,29 @@
+package sample;
+
+/**
+ * Created by benjamintoofer on 7/6/14.
+ */
+public interface TestConstants {
+
+    //Scene Height and Width
+    int SCENE_WIDTH = 400;
+    int SCENE_HEIGHT = 400;
+
+    //Number of coloumns
+    int NUM_COL = 8;
+
+    //Number of rows
+    int NUM_ROW = 8;
+
+    //Table height
+    int HEIGHT = 100;
+
+    //Table width
+    int WIDTH = 100;
+
+    //speed of fruits animating
+    double SWITCH_SPEED = 3;
+
+    double RADIUS = (SCENE_HEIGHT/NUM_ROW)/4;
+
+}
