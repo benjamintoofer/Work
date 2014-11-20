46fa8e2505f6fcea6c0b4aa2fc8b305d01c491f8
Benjamin Toofer <benjamintoofer@gmail.com>
2014-11-20 02:28:03 -0800

    updating stuff

---
+++ Java/Snake/src/Main.java
@@ -0,0 +1,63 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * Created by benjamintoofer on 11/15/14.
+ */
+public class Main extends Application{
+
+    private final int WIDTH = 900;
+    private final int HEIGHT = 720;
+    private final RenderSystem ren = new RenderSystem(WIDTH,HEIGHT);
+    private final GraphicsContext gc = ren.getGraphicsContext2D();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Group root = new Group();
+        root.getChildren().add(ren);
+        //root.getChildren().add(new Button("Button"));
+
+        primaryStage.setTitle("Snake");
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT, Color.WHITE));
+        primaryStage.show();
+
+        Thread gameThread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try{
+                    while(true)
+                    {
+                        ren.tick();
+
+                        //ren.render(gc);
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run()
+                            {
+                                ren.render(gc);
+                            }
+                        });
+
+                        Thread.sleep(60);
+                    }
+                }catch(InterruptedException ex){}
+            }
+        });
+        gameThread.start();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+        System.exit(0);
+    }
+}
---
+++ Java/Snake/src/SnakeSegment.java
@@ -0,0 +1,14 @@
+/**
+ * Created by benjamintoofer on 11/16/14.
+ */
+public class SnakeSegment {
+
+    public int xPos,yPos;
+
+    public SnakeSegment(int xPos,int yPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+}
