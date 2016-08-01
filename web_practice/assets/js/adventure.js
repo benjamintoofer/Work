891f4f94fb504d772b1678521b366250acfd0f29
Benjamin Toofer <benjamintoofer@gmail.com>
2016-08-01 01:16:39 -0600

    finished scrolling through gallery by click. maybe add arrow keys

--- web_practice/assets/js/adventure.js
+++ web_practice/assets/js/adventure.js
@@ -7,6 +7,7 @@ window.onload = function() {
 //Global variables
 var current_image_index;
 var prev_image_index;
+var next_image_index;
 var list_of_images;
 var timeout;
 
@@ -17,12 +18,29 @@ function init()
     add_click_open_overlay();
     add_click_close_overlay();
     add_move_mouse_overlay();
+    add_click_scroll_gallery();
+    add_click_prev_image();
     add_click_next_image();
 }
 
 // Event Handlers
+function image_scroll(event)
+{
+        if(event.clientX > window.innerWidth/2){
+            prev_image();
+        }else{
+            next_image();
+        }
+}
+function add_click_scroll_gallery()
+{
+    $('click_screen').addEventListener('click',image_scroll);
+}
+function add_click_prev_image(){
+    $('right_bttn').addEventListener("click",prev_image);
+}
 function add_click_next_image(){
-    $('right_bttn').addEventListener("click",next_image);
+    $('left_bttn').addEventListener("click",next_image);
 }
 function add_click_open_overlay()
 {
@@ -44,10 +62,10 @@ function add_move_mouse_overlay()
     // $('photo_page').addEventListener("mousemove",show_button);
     $('photo_page').addEventListener("mousemove",show_button);
 }
-
-function next_image()
+var timeout_2
+function prev_image()
 {
-    console.log(current_image_index);
+
     var curr = $('displayed_image_container');
     var prev = $('prev_image_container');
 
@@ -55,18 +73,46 @@ function next_image()
     prev.setAttribute('class','move_right');
     var cont_new = $('.move_right');
     var duration = Number(window.getComputedStyle(curr).transitionDuration.split("s")[0]);
-    console.log(window.getComputedStyle(cont_new[0]).transitionDuration);
-    console.log(duration);
-    clearTimeout(timeout);
-    timeout = setTimeout(function(){
+
+    clearTimeout(timeout_2);
+    timeout_2 = setTimeout(function(){
 
         current_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
         prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
+        next_image_index = ((current_image_index + 1) + list_of_images.length) % list_of_images.length;
+
         load_prev_image(prev_image_index);
         load_current_image(current_image_index);
+        load_next_image(next_image_index);
 
         curr.setAttribute('class','');
         prev.setAttribute('class','');
+    }, 1000 * duration);
+}
+
+function next_image()
+{
+    var curr = $('displayed_image_container');
+    var next = $('next_image_container');
+
+    curr.setAttribute('class','move_left');
+    next.setAttribute('class','move_left');
+    var cont_new = $('.move_left');
+    var duration = Number(window.getComputedStyle(curr).transitionDuration.split("s")[0]);
+
+    clearTimeout(timeout_2);
+    timeout_2 = setTimeout(function(){
+
+        current_image_index = ((current_image_index + 1) + list_of_images.length) % list_of_images.length;
+        prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
+        next_image_index = ((current_image_index + 1) + list_of_images.length) % list_of_images.length;
+        
+        load_prev_image(prev_image_index);
+        load_current_image(current_image_index);
+        load_next_image(next_image_index);
+
+        curr.setAttribute('class','');
+        next.setAttribute('class','');
     }, 1000 * duration);
 }
 
@@ -100,12 +146,12 @@ function open_image()
 {
     //Current image info
     current_image_index = Number(this.id.split('_')[1]) - 1;
-
-    //Previous Image info
-    var prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
+    prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
+    next_image_index = ((current_image_index + 1) + list_of_images.length) % list_of_images.length;
 
     load_current_image(current_image_index);
     load_prev_image(prev_image_index);
+    load_next_image(next_image_index);
 
     open_overlay();
 
@@ -113,7 +159,7 @@ function open_image()
 }
 function load_current_image(index)
 {
-    current_image = list_of_images[index];
+    var current_image = list_of_images[index];
     var get_curr_img = current_image.getElementsByTagName('img')[0];
     var img_src = get_curr_img.getAttribute("src");
     var img_to_display_div = $('displayed_image');
@@ -124,13 +170,25 @@ function load_current_image(index)
 function load_prev_image(index)
 {
     // var prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
-    prev_image = list_of_images[index];
+    var prev_image = list_of_images[index];
     var get_prev_img = prev_image.getElementsByTagName('img')[0];
     var prev_img_src = get_prev_img.getAttribute("src");
     var prev_img_to_display_div = $('prev_image');
     var prev_img_to_display = prev_img_to_display_div.getElementsByTagName('img')[0];
 
     prev_img_to_display.setAttribute("src", prev_img_src);
+}
+
+function load_next_image(index)
+{
+    // var prev_image_index = ((current_image_index - 1) + list_of_images.length) % list_of_images.length;
+    var next_image = list_of_images[index];
+    var get_next_img = next_image.getElementsByTagName('img')[0];
+    var next_img_src = get_next_img.getAttribute("src");
+    var next_img_to_display_div = $('next_image');
+    var next_img_to_display = next_img_to_display_div.getElementsByTagName('img')[0];
+
+    next_img_to_display.setAttribute("src", next_img_src);
 }
 function open_overlay()
 {
